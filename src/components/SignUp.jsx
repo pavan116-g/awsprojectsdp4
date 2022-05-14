76ae98da4d6c162495d9bbd07949ea8ba286fced
pavan116-g <indragantiteja352@gmail.com>
2022-05-14 11:41:58 +0530

    ss

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -19,25 +19,25 @@ const Signup = () =>{
     };
 
     return(
-        <div>
-            <center>
-                <h1>Registration</h1>
-                <br></br><br></br><br></br>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="email">Email</label>
-                <input
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}></input>
+            <div>
+                <center>
+                    <h1>Registration</h1>
+                    <br></br><br></br><br></br>
+                <form onSubmit={onSubmit}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}></input>
 
-                <label htmlFor="password">Password</label>
-                <input
-                    value={password}
-                    onChange={(event) => setpassword(event.target.value)}></input>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        value={password}
+                        onChange={(event) => setpassword(event.target.value)}></input>
 
-                   <button type="submit">Submit</button> 
-            </form>
-            </center>
-        </div>
+                    <button type="submit">Submit</button> 
+                </form>
+                </center>
+            </div>
     );
 };
 
